a2244df907070b574055001102052ed4f657c108
Felix Itzenplitz <felix@stkn.org>
2015-07-21 12:42:41 +0200

    fix(h5vzr): update zoom

--- app/scripts/services/zoom.js
+++ app/scripts/services/zoom.js
@@ -9,6 +9,8 @@
  */
 angular.module('loggrioApp')
   .service('zoom', function () {
+    var self = this;
+
     var MONTH = 30 * 24 * 3600 * 1000;
     var WEEK = 7 * 24 * 3600 * 1000;
     var DAY = 24 * 3600 * 1000;
@@ -44,65 +46,60 @@ angular.module('loggrioApp')
       }
     ];
 
-    this.getZoomNavigation = function (charts) {
-      return {
-        selectRange: function (chartIndex, value) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          var max = chart.series[0].xAxis.dataMax;
-          var min = max - value;
-          chart.xAxis[0].setExtremes(min, max);
-        },
-        zoomIn: function (chartIndex) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          var extremes = chart.xAxis[0].getExtremes();
-          var max = extremes.max;
-          var min = extremes.min;
-          chart.xAxis[0].setExtremes(min + singleStep(min,max), max - singleStep(min,max));
-        },
-        zoomOut: function (chartIndex) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          var extremes = chart.xAxis[0].getExtremes();
-          var max = extremes.max;
-          var min = extremes.min;
-          chart.xAxis[0].setExtremes(min - singleStep(min,max), max + singleStep(min,max));
-        },
-        navigateLeft: function (chartIndex) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          var extremes = chart.xAxis[0].getExtremes();
-          var max = extremes.max;
-          var min = extremes.min;
-          chart.xAxis[0].setExtremes(min - singleStep(min,max), max - singleStep(min,max));
-        },
-        navigateRight: function (chartIndex) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          var extremes = chart.xAxis[0].getExtremes();
-          var max = extremes.max;
-          var min = extremes.min;
-          chart.xAxis[0].setExtremes(min + singleStep(min,max), max + singleStep(min,max));
-        },
-        resetZoom: function (chartIndex) {
-          var chart = charts[chartIndex].default.getHighcharts();
-          if (!chart) {
-            return;
-          }
-          chart.zoomOut();
-        }
-      };
+    this.selectRange = function (chart, value) {
+      if (!chart) {
+        return;
+      }
+      var max = chart.series[0].xAxis.dataMax;
+      var min = max - value;
+      chart.xAxis[0].setExtremes(min, max);
+    };
+
+    this.zoomIn = function (chart) {
+      if (!chart) {
+        return;
+      }
+      var extremes = chart.xAxis[0].getExtremes();
+      var max = extremes.max;
+      var min = extremes.min;
+      chart.xAxis[0].setExtremes(min + singleStep(min, max), max - singleStep(min, max));
+    };
+
+    this.zoomOut = function (chart) {
+      if (!chart) {
+        return;
+      }
+      var extremes = chart.xAxis[0].getExtremes();
+      var max = extremes.max;
+      var min = extremes.min;
+      chart.xAxis[0].setExtremes(min - singleStep(min, max), max + singleStep(min, max));
+    };
+
+    this.navigateLeft = function (chart) {
+      if (!chart) {
+        return;
+      }
+      var extremes = chart.xAxis[0].getExtremes();
+      var max = extremes.max;
+      var min = extremes.min;
+      chart.xAxis[0].setExtremes(min - singleStep(min, max), max - singleStep(min, max));
+    };
+
+    this.navigateRight = function (chart) {
+      if (!chart) {
+        return;
+      }
+      var extremes = chart.xAxis[0].getExtremes();
+      var max = extremes.max;
+      var min = extremes.min;
+      chart.xAxis[0].setExtremes(min + singleStep(min, max), max + singleStep(min, max));
+    };
+
+    this.resetZoom = function (chart) {
+      if (!chart) {
+        return;
+      }
+      chart.zoomOut();
     };
 
     this.shift = function (chart, lastPoint) {
@@ -116,5 +113,28 @@ angular.module('loggrioApp')
       var beforeLastPoint = xData[xData.length - 1];
       var diff = lastPoint - beforeLastPoint;
       chart.xAxis[0].setExtremes(min + diff, max + diff, false);
+    };
+
+    this.getZoomNavigation = function (charts) {
+      return {
+        selectRange: function (chartIndex, value) {
+          self.selectRange(charts[chartIndex].default.getHighcharts(), value);
+        },
+        zoomIn: function (chartIndex) {
+          self.zoomIn(charts[chartIndex].default.getHighcharts());
+        },
+        zoomOut: function (chartIndex) {
+          self.zoomOut(charts[chartIndex].default.getHighcharts());
+        },
+        navigateLeft: function (chartIndex) {
+          self.navigateLeft(charts[chartIndex].default.getHighcharts());
+        },
+        navigateRight: function (chartIndex) {
+          self.navigateRight(charts[chartIndex].default.getHighcharts());
+        },
+        resetZoom: function (chartIndex) {
+          self.resetZoom(charts[chartIndex].default.getHighcharts());
+        }
+      };
     };
   });
