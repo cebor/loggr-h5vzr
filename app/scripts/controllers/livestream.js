0213609e2a97f697600e75865c00f9b79d0a100b
Marcel <marcelpuetz@web.de>
2015-11-03 18:56:07 +0100

    feat: demo livestream, toolbar layout

---
+++ app/scripts/controllers/livestream.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loggrioApp.controller:LivestreamCtrl
+ * @description
+ * # LivestreamCtrl
+ * Controller of the loggrioApp
+ */
+angular.module('loggrioApp')
+  .controller('LivestreamCtrl', function ($rootScope) {
+    $rootScope.header = 'Livestream';
+  });
