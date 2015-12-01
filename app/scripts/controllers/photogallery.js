b1ba9171cc53dc437b827603f1ef514ca31afdbb
SimoKutlin <simo.kutlin@gmail.com>
2015-12-01 02:03:30 +0100

    feat: add coverflow to gallery

--- app/scripts/controllers/photogallery.js
+++ app/scripts/controllers/photogallery.js
@@ -17,20 +17,28 @@ angular.module('loggrioApp')
       date: '2015-10-31T03:13:57+00:00',
       downloaded: true,
       images: [
-        {thumb: 'images/photogallery/hallo1.png', img: 'images/photogallery/hallo1.png'},
-        {thumb: 'images/photogallery/hallo2.jpg', img: 'images/photogallery/hallo2.jpg'},
-        {thumb: 'images/photogallery/hallo3.jpg', img: 'images/photogallery/hallo3.jpg'},
-        {thumb: 'images/photogallery/hallo4.jpg', img: 'images/photogallery/hallo4.jpg'}
-      ]},{
+        'images/photogallery/hallo1.png',
+        'images/photogallery/hallo2.jpg',
+        'images/photogallery/hallo3.jpg',
+        'images/photogallery/hallo4.jpg',
+        'images/photogallery/image1.jpg',
+        'images/photogallery/image2.jpg',
+        'images/photogallery/image3.jpg',
+        'images/photogallery/image4.jpg'
+      ],
+      coverflow: {}
+    },{
+      flowid: 'coverflow2',
       date: '2015-11-02T15:50:57+00:00',
       downloaded: false,
       images: [
-        {thumb: 'images/photogallery/image1.jpg', img: 'images/photogallery/image1.jpg'},
-        {thumb: 'images/photogallery/image2.jpg', img: 'images/photogallery/image2.jpg'},
-        {thumb: 'images/photogallery/image3.jpg', img: 'images/photogallery/image3.jpg'},
-        {thumb: 'images/photogallery/image4.jpg', img: 'images/photogallery/image4.jpg'}
-      ]}
-    ];
+        'images/photogallery/image1.jpg',
+        'images/photogallery/image2.jpg',
+        'images/photogallery/image3.jpg',
+        'images/photogallery/image4.jpg',
+      ],
+      coverflow: {}
+    }];
 
     /*
      * checks if photos from chosen shot have already been downloaded,
