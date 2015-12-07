faa658fbdaf3719e46a1ae931cf8316f5013558b
Marcel <marcelpuetz@web.de>
2015-12-07 22:48:44 +0100

    feat: stretch stream to card width, stream is not heigher than remaining space

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,4 +7,4 @@ angular
   .module('loggrioApp')
     .constant('API', 'http://localhost:3000/api')
     .constant('POLLING_INTERVAL', 10000)
-    .constant('STREAM_URL','http://141.60.125.150:8080/?action=stream');
+    .constant('STREAM_URL','http://141.60.125.254:8080/?action=stream');
